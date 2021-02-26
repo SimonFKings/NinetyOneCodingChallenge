3d55772428c09d0f5b5dd784eacdcd5ed07ed127
SimonFKings <Gbamila1>
2021-02-26 01:33:31 +0000

    Added comments to code

--- src/NinetyOne.java
+++ src/NinetyOne.java
@@ -12,18 +12,22 @@ public class NinetyOne {
 
 
 public static void convertToWords(String filename){
-    try {
+
+    try
+    {
+        //Scanner used to check each line of the file
         Scanner fileScanner = new Scanner(new File(filename));
         while(fileScanner.hasNextLine()) {
+            //A new scanner to check individual lines
             String line = fileScanner.nextLine();
             Scanner lineScanner  = new Scanner(line);
+            //Check if an input is valid from the line
             boolean valid = false;
-            while (lineScanner.hasNext()) {
 
-//                long number = Long.parseLong(line.replaceAll("\\D+", ""));
+            while (lineScanner.hasNext()) {
                 if (lineScanner.hasNextLong()) {
                     long input = lineScanner.nextLong();
-
+                    //Check if the next input contains another number to ensure this input is valid
                     String next = lineScanner.next();
                     if(!next.matches(".*\\d.*")) {
                         System.out.println(getWord(input));
@@ -73,21 +77,28 @@ public static void convertToWords(String filename){
             return units[(int) n];
         }
 
+        //The condition checks if the a second word is needed then adds a dash if so
         if (n < 100) {
             return tens[(int) (n / 10)] + ((n % 10 != 0) ? "-" : "") + units[(int) (n % 10)];
         }
 
+        // The checks if an and word is needed after the hundred word
         if (n < 1000) {
             return units[(int) (n / 100)] + " hundred" + ((n % 100 != 0) ? " and " : "") + getWord(n % 100);
         }
 
+
+        //Check if the next word contains an and, if so add a comma. If the current word is before the last, add an "and"
         if (n < 1000000) {
             String result = getWord(n / 1000) + " thousand" ;
             if(getWord(n % 1000).contains("and")){
-                result = result + ", " + getWord(n % 1000);
-            }else
-                result = result + " and " +getWord(n%1000);
-            return  result;
+                return result + ", " + getWord(n % 1000);
+            }else if (n % 1000 != 0 )
+                return  result + " and " +getWord(n%1000);
+                else
+                    return  result;
+
+
 
         }
 
@@ -95,22 +106,21 @@ public static void convertToWords(String filename){
             String result = getWord(n / 1000000) + " million";
 
             if(getWord(n % 1000000).contains("and"))
-                result = result + ", " + getWord(n % 1000000);
+                return result + ", " + getWord(n % 1000000);
+            else if (n % 1000000 != 0)
+                return  result + " and " + getWord(n % 1000000);
             else
-                result = result + " and " + getWord(n % 1000000);
-            return result;
+                return result;
 
-//            return getWord(n / 1000000) + " million" + ((n % 1000000 != 0) ? " " : "") + getWord(n % 1000000);
         }
 
 
         String result = getWord(n / 1000000000) + " billion";
         if(getWord(n % 1000000000).contains("and"))
-            result = result + ", " + getWord(n % 1000000000);
+            return result + ", " + getWord(n % 1000000000);
+        else if(n % 1000000000 != 0)
+            return result + " and " + getWord(n % 1000000000);
         else
-            result = result + " and " + getWord(n % 1000000000);
-
-        return result;
-              //  + ((n % 1000000000 != 0) ? " " : "") + getWord(n % 1000000000);
+            return result;
     }
 }
